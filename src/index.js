208fd243b68415e85bacc7d31d7a6d322ffc3866
Sakshi <sakshi35sin@gmail.com>
2024-02-28 23:32:57 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from 'react-dom';
+//React library is used to create the element
+let element = React.createElement('h1',null,'Hello World');
+//React Dom is used to render the created elemnt at spectfied position
+ReactDOM.render(element,document.querySelector('#root'));
