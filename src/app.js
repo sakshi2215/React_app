4ad1a7e32c148967aa390140640dd21c40c5e718
Sakshi <sakshi35sin@gmail.com>
2024-02-29 22:47:46 +0530

    Ok

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-
+import Jumbotron from "./jumbotron.js";
+import Navbar from "./navbar.js";
+import React,{Component} from "react";
+class App extends Component{
+     render(){
+        return(
+            <div>
+                <Navbar/>
+                <Jumbotron/>
+            </div>
+            
+        );
+     }
+}
+export default App;
---
+++ src/navbar.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Navbar extends Component{
+    render(){
+        return(
+            <nav className="navbar bg-danger">
+                <a href="#" className="navbar-brand text-white">Flipkart</a>
+            </nav>
+        );
+    }
+}
+export default Navbar;
