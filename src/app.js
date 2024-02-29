aaaed89f9986a80fbbe5e55c1a912a46b07b7554
Sakshi <sakshi35sin@gmail.com>
2024-02-29 21:42:33 +0530

    Day -02

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+//Functional and class compnents
+
+//React Component
+class App extends Component{
+    //app ki properties kya hai
+    state ={};
+    //ye componnt dikhayi kya dega
+    render(){
+       return(<h1 className="blue-text">Hello World</h1>);
+    }
+}
+
+export default App;
